063f6c7f1770318d1f37f964de3156e7b725aac2
Matúš Pytel <matuspytel@gmail.com>
2025-03-30 15:25:00 +0200

    simplify dialog

--- src/gui/Dialog.java
+++ src/gui/Dialog.java
@@ -38,26 +38,23 @@ public class Dialog extends SelectOption {
     public void draw(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         g2.setStroke(new BasicStroke(5));
-
         g2.setColor(new Color(0, 0, 0, 140));
         g2.fillRoundRect(400, 250, 300, 200, 15, 15);
         g2.setColor(new Color(255, 255, 255));
         g2.drawRoundRect(400, 250, 300, 200, 15, 15);
-        if (this.chosenOption == ConfirmDialog.YES) {
-            g2.setColor(new Color(255, 255, 255));
-            g2.setStroke(new BasicStroke(5));
-            g2.drawRoundRect(450, 360, 80, 50, 15, 15);
-            g2.setStroke(new BasicStroke(2));
 
-            g2.drawRoundRect(570, 360, 80, 50, 15, 15);
-        } else {
-            g2.setStroke(new BasicStroke(2));
-            g2.setColor(new Color(255, 255, 255));
-            g2.drawRoundRect(450, 360, 80, 50, 15, 15);
-            g2.setStroke(new BasicStroke(5));
-            g2.drawRoundRect(570, 360, 80, 50, 15, 15);
+        g2.setStroke(new BasicStroke(2));
+        g2.drawRoundRect(450, 360, 80, 50, 15, 15);
+        g2.drawRoundRect(570, 360, 80, 50, 15, 15);
+        g2.setStroke(new BasicStroke(5));
+        switch (this.chosenOption) {
+            case YES -> {
+                g2.drawRoundRect(450, 360, 80, 50, 15, 15);
+            }
+            case NO -> {
+                g2.drawRoundRect(570, 360, 80, 50, 15, 15);
+            }
         }
-
         g2.setFont(new Font("Consolas", Font.BOLD, 30));
         switch (this.playState) {
             case WIN -> g2.drawString("You Won!", 480, 290);
